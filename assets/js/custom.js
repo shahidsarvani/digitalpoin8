d8c346dbc2d74ee8c0b9e7d08204d129d6438509
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-02-28 18:01:37 +0500

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,11 +10,10 @@ $(document).ready(function () {
   })
 
   $('.projects_slider').on('init', function (event, slick) {
-    console.log(slick)
     $(slick.$slides[0]).addClass('main-slide')
     $(slick.$slides[1]).addClass('right-slide')
-    if($('.projects_slider div.slick-slide.slick-cloned.slick-active').attr('data-slick-index') == -1) {
-        $('.projects_slider div.slick-slide.slick-cloned.slick-active').addClass('left-slide')
+    if ($('.projects_slider div.slick-slide.slick-cloned.slick-active').attr('data-slick-index') == -1) {
+      $('.projects_slider div.slick-slide.slick-cloned.slick-active').addClass('left-slide')
     }
   })
   $('.projects_slider')
@@ -25,14 +24,20 @@ $(document).ready(function () {
       speed: 300,
       slidesToShow: 3,
       adaptiveHeight: true,
-    //   infinite: false
+      //   infinite: false
     })
     .on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-        $('.projects_slider .slick-slide').removeClass('right-slide').removeClass('left-slide')
+      $('.projects_slider .slick-slide').removeClass('right-slide').removeClass('left-slide')
       $(slick.$slides[currentSlide]).removeClass('main-slide')
       $(slick.$slides[nextSlide]).removeClass('left-slide').removeClass('right-slide').addClass('main-slide')
       $(slick.$slides[nextSlide + 1]).addClass('right-slide')
       $(slick.$slides[nextSlide - 1]).addClass('left-slide')
-
+    }).on('afterChange', function (event, slick, currentSlide, nextSlide) {
+      if ($('.projects_slider div.slick-slide.slick-cloned.slick-active').attr('data-slick-index') == -1) {
+        $('.projects_slider div.slick-slide.slick-cloned.slick-active').addClass('left-slide')
+      }
+      if ($('.projects_slider div.slick-slide.slick-cloned.slick-active').attr('data-slick-index') == 4) {
+        $('.projects_slider div.slick-slide.slick-cloned.slick-active').addClass('right-slide')
+      }
     })
 })
