e7e2fd8c24e3dcea3325701436c664f560a90ba1
flopmart <34133163+asadnisar98@users.noreply.github.com>
2022-02-28 17:38:45 +0500

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,24 +1,38 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $('.client_slider').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    centerMode: true,
+    speed: 300,
+    slidesToShow: 4,
+    adaptiveHeight: true,
+  })
 
+  $('.projects_slider').on('init', function (event, slick) {
+    console.log(slick)
+    $(slick.$slides[0]).addClass('main-slide')
+    $(slick.$slides[1]).addClass('right-slide')
+    if($('.projects_slider div.slick-slide.slick-cloned.slick-active').attr('data-slick-index') == -1) {
+        $('.projects_slider div.slick-slide.slick-cloned.slick-active').addClass('left-slide')
+    }
+  })
+  $('.projects_slider')
+    .slick({
+      dots: false,
+      arrows: false,
+      centerMode: true,
+      speed: 300,
+      slidesToShow: 3,
+      adaptiveHeight: true,
+    //   infinite: false
+    })
+    .on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+        $('.projects_slider .slick-slide').removeClass('right-slide').removeClass('left-slide')
+      $(slick.$slides[currentSlide]).removeClass('main-slide')
+      $(slick.$slides[nextSlide]).removeClass('left-slide').removeClass('right-slide').addClass('main-slide')
+      $(slick.$slides[nextSlide + 1]).addClass('right-slide')
+      $(slick.$slides[nextSlide - 1]).addClass('left-slide')
 
-    $('.client_slider').slick({
-        dots: false,
-        arrows: false,
-        infinite: true,
-        centerMode: true,
-        speed: 300,
-        slidesToShow: 4,
-        adaptiveHeight: true
-      });
-
-      
-      $('.projects_slider').slick({
-        dots: false,
-        arrows: false,
-        speed: 300,
-        slidesToShow: 3,
-        adaptiveHeight: true
-      });
-
-
-  });
+    })
+})
