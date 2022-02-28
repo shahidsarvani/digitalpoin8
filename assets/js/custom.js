e56c744b55491a853e71e60003cc3961cc031aa0
flopmart <34133163+asadnisar98@users.noreply.github.com>
2022-02-28 14:04:40 +0500

    new heading

---
+++ assets/js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+
+    $('.client_slider').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+      });
+
+
+  });
